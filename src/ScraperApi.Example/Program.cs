bc8b034c90f893ba3d0c7a8f7bfd775849b4fc6a
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-03 10:28:33 +1000

    Added Example project.

--- src/ScraperApi.Example/Program.cs
+++ src/ScraperApi.Example/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using ScraperApi;
 
-namespace ScraperApi.Example
+namespace Example
 {
     internal static class Program
     {
         private static async Task Main()
         {
             using var client = new HttpClient();
-            var api = new ScraperApi(client);
+            var apiKey = Environment.GetEnvironmentVariable("SCRAPER_API_TOKEN") ?? string.Empty;
+            var api = new ScraperApiClient(apiKey, client);
 
-            var response = await api.GetCurrentInformationAsync();
+            var result = await api.GetAsync("http://httpbin.org/ip");
 
-            Console.WriteLine($"City: {response.City}");
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
