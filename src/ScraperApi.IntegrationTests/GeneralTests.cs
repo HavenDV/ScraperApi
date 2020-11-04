0029692ce53e559866343f85484a8ea9505b0f84
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-04 19:41:04 +1000

    Added sessions test.

--- src/ScraperApi.IntegrationTests/GeneralTests.cs
+++ src/ScraperApi.IntegrationTests/GeneralTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using ScraperApi.IntegrationTests.Utilities;
@@ -15,6 +16,22 @@ namespace ScraperApi.IntegrationTests
         [TestMethod]
         public async Task PremiumGetAsyncTest() => await BaseTests.TextTestAsync(
             (api, cancellationToken) => api.GetAsync("http://httpbin.org/ip", premium: true, cancellationToken: cancellationToken));
+
+        [TestMethod]
+        public async Task SessionsGetAsyncTest() => await BaseTests.TextTestAsync(
+            async (api, cancellationToken) =>
+            {
+                const string? id = "123";
+                var response1 = await api.GetAsync("http://httpbin.org/ip", sessionNumber: id,
+                    cancellationToken: cancellationToken);
+
+                var response2 = await api.GetAsync("http://httpbin.org/ip", sessionNumber: id,
+                    cancellationToken: cancellationToken);
+
+                Assert.AreEqual(response1, response2, "Responses are not equal");
+
+                return response1 + Environment.NewLine + response2;
+            });
 
         [TestMethod]
         public async Task CustomHeadersGetAsyncTest() => await BaseTests.TextTestAsync(
